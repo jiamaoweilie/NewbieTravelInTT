ea4764c88a57b90a6f58239536b8896553cb23a6
wzhao0928 <wbzhao@thoughtworks.com>
2015-03-04 18:15:43 +0800

    main page modification for the new theme

--- scripts/lightbox.js
+++ scripts/lightbox.js
@@ -6,7 +6,7 @@ var lightbox = {
 		var countTasksAccepted = $("#info-tasks-accepted").attr("value");
 		var isAccepted = $(taskMapImg).attr("value");
 
-		$("#lb-task-type").html($(taskMapImg).parent().siblings(".task_map_title").text());
+		$("#lb-task-type").html($(taskMapImg).parent().parent().children(".task_map_header").children(".task_map_title").text());
 
         $("#sect-light-box").fadeIn("slow");
         $("#light-box-bg").fadeIn("slow");
@@ -20,13 +20,16 @@ var lightbox = {
         yButton.bind("click", taskMapImg, function(image){
         	var accepted_tasks = $(".accepted_task");
         	var i;
+        	var imgUrl = $(image.data).attr("src");
 	        image.preventDefault();
 	        $("#sect-light-box").fadeOut("slow");
 	        $("#light-box-bg").fadeOut("slow");
+
 	        $(image.data).siblings(".mark_in_progress").show();
+	        $(image.data).parent().css("background-color", "#333");
 	        for (i = 0; i < accepted_tasks.length; i++) {
 	        	if (0 === $(accepted_tasks[i]).children().length) {
-	        		$(accepted_tasks[i]).append('<img src="image/sample.jpg" alt="" class="img_task_accepted"/>');
+	        		$(accepted_tasks[i]).append('<img src="'+ imgUrl +'" alt="" class="img_task_accepted"/>');
 	        		countTasksAccepted = '' + (i + 1);
 	        		$("#info-tasks-accepted").attr("value", "" + (i + 1));
 	        		$(image.data).attr("value", "ACCEPTED");
